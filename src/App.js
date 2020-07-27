c85b5d8dc94a6e9270d707e29d1c2d8a0bc405a7
abdullah706 <65893886+abdullah706@users.noreply.github.com>
2020-07-28 00:01:59 +0200

    Commit message

--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends React.Component {
+
+  state = {
+    name: '',
+    capital: '',
+    flag: '',
+    population: 0,
+    region: '',
+    flag: ''
+  }
+
+  componentDidMount() {
+    fetch('https://restcountries.eu/rest/v2/name/france', { method: 'GET'} )
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          name: json[0].name,
+          capital: json[0].capital,
+          region: json[0].region,
+          population: json[0].population,
+          flag: json[0].flag
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <img src={this.state.flag} style={{height: '100px', width: '150px'}}/>
+        <p>Country: {this.state.name}</p>
+        <p>Capital: {this.state.capital}</p>
+        <p>Region: {this.state.region}</p>
+        <p>Population: {this.state.population}</p>
+      </div>
+    )
+  }
+
 }
 
 export default App;
